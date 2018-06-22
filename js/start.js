281b231b727623fe521a0bf028c714b0427cbdd0
João Williams Balaniúc <jwillbala@hotmail.com>
2018-06-22 11:55:49 -0300

    “beta”

--- js/start.js
+++ js/start.js
@@ -45,15 +45,22 @@ var phonegap = {
 
     alert(1);
 
-    SmsReceiver.isSupported((supported) => {
-      if (supported) {
-        alert("SMS supported!")
-      } else {
-        alert("SMS not supported")
-      }
-    }, () => {
-      alert("Error while checking the SMS support")
-    })
+    var number = "28999652165";
+    var message = "teste";
+
+    //CONFIGURATION
+    var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+            intent: 'INTENT'  // send SMS with the native android SMS messaging
+            //intent: '' // send SMS without open any other app
+        }
+    };
+    var success = function () { alert('Message sent successfully'); };
+    var error = function (e) { alert('Message Failed:' + e); };
+    sms.send(number, message, options, success, error);
+
+    alert(2);
 
     // GPS enabled?
     /*
