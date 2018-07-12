170ec2560db19f1e942f512f998a65ed6ab60e04
João Williams Balaniúc <jwillbala@hotmail.com>
2018-07-11 22:20:39 -0300

    “beta”

--- www/js/app.js
+++ www/js/app.js
@@ -106,94 +106,74 @@ var phonegap = {
 
     // razor data
     start();
+    //
+    //
+    // after deviceready
+    //
+    //
     alert(0);
-    BackgroundGeolocation.configure({
-      desiredAccuracy: 10,
-      stationaryRadius: 50,
-      distanceFilter: 50,
-      notificationTitle: 'Background tracking',
-      notificationText: 'enabled',
-      debug: true,
-      startOnBoot: false,
-      stopOnTerminate: false,
-      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
-      interval: 10000,
-      fastestInterval: 5000,
-      activitiesInterval: 10000,
-      stopOnStillActivity: false,
-      url: 'http://nickford.com.br/razor/ws.php',
-      httpHeaders: {
-        'X-FOO': 'bar'
-      }
+    // Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
+    //  in order to prompt the user for Location permission.
+    window.navigator.geolocation.getCurrentPosition(function(location) {
+      console.log('Location from Phonegap');
+      ws("geo 0");
     });
     alert(1);
-    BackgroundGeolocation.on('location', (location) => {
-      // handle your locations here
-      // to perform long running operation on iOS
-      // you need to create background task
-      BackgroundGeolocation.startTask(taskKey => {
-        // execute long running task
-        // eg. ajax post location
-        // IMPORTANT: task has to be ended by endTask
-        ws("bla");
-        BackgroundGeolocation.endTask(taskKey);
-      });
-    });
-
-    BackgroundGeolocation.on('stationary', (stationaryLocation) => {
-      // handle stationary locations here
-      Actions.sendLocation(stationaryLocation);
-    });
-
-    BackgroundGeolocation.on('error', (error) => {
-      ws("error");
-      console.log('[ERROR] BackgroundGeolocation error:', error);
-    });
-
-    BackgroundGeolocation.on('start', () => {
-      ws("start");
-      console.log('[INFO] BackgroundGeolocation service has been started');
-    });
-
-    BackgroundGeolocation.on('stop', () => {
-      ws("stop");
-      console.log('[INFO] BackgroundGeolocation service has been stopped');
-    });
-
-    BackgroundGeolocation.on('authorization', (status) => {
-      ws("auth: "+JSON.stringify(status));
-      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
-      if (status !== BackgroundGeolocation.AUTHORIZED) {
-        Alert.alert('Location services are disabled', 'Would you like to open location settings?', [
-          { text: 'Yes', onPress: () => BackgroundGeolocation.showLocationSettings() },
-          { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
-        ]);
-      }
-    });
-
-    BackgroundGeolocation.on('background', () => {
-      ws("background");
-      console.log('[INFO] App is in background');
-    });
-
-    BackgroundGeolocation.on('foreground', () => {
-      ws("foreground");
-      console.log('[INFO] App is in foreground');
-    });
+    var bgGeo = window.plugins.backgroundGeoLocation;
     alert(2);
-    BackgroundGeolocation.checkStatus(status => {
-      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
-      console.log('[INFO] BackgroundGeolocation service has permissions', status.hasPermissions);
-      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
-
-      // you don't need to check status before start (this is just the example)
-      if (!status.isRunning) {
-        BackgroundGeolocation.start(); //triggers start on start event
-      }
-    });
-
-    BackgroundGeolocation.start();
+    /**
+    * This would be your own callback for Ajax-requests after POSTing background geolocation to your server.
+    */
+    var yourAjaxCallback = function(response) {
+      ////
+      // IMPORTANT:  You must execute the #finish method here to inform the native plugin that you're finished,
+      //  and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+      // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+      //
+      //
+      ws("response...");
+      bgGeo.finish();
+    };
     alert(3);
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+    var callbackFn = function(location) {
+      ws('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+      console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+      // Do your HTTP request here to POST location to your server.
+      //
+      //
+      yourAjaxCallback.call(this);
+    };
+
+    var failureFn = function(error) {
+      console.log('BackgroundGeoLocation error');
+    }
+    alert(4);
+    // BackgroundGeoLocation is highly configurable.
+    bgGeo.configure(callbackFn, failureFn, {
+      url: 'http://only.for.android.com/update_location.json', // <-- Android ONLY:  your server url to send locations to
+      params: {
+        auth_token: 'user_secret_auth_token',    //  <-- Android ONLY:  HTTP POST params sent to your server when persisting locations.
+        foo: 'bar'                              //  <-- Android ONLY:  HTTP POST params sent to your server when persisting locations.
+      },
+      headers: {                                   // <-- Android ONLY:  Optional HTTP headers sent to your configured #url when persisting locations
+        "X-Foo": "BAR"
+      },
+      desiredAccuracy: 10,
+      stationaryRadius: 20,
+      distanceFilter: 30,
+      notificationTitle: 'Background tracking', // <-- android only, customize the title of the notification
+      notificationText: 'ENABLED', // <-- android only, customize the text of the notification
+      activityType: 'AutomotiveNavigation',
+      debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+      stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+    });
+    alert(5);
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    bgGeo.start();
+    alert(6);
     phonegap.receivedEvent('deviceready');
 
   },
@@ -231,4 +211,98 @@ var success = function () { alert('Message sent successfully'); };
 var error = function (e) { alert('Message Failed:' + e); };
 //sms.send(number, message, options, success, error);
 //alert(2);
+*/
+
+
+
+/*
+/*
+alert(0);
+BackgroundGeolocation.configure({
+desiredAccuracy: 10,
+stationaryRadius: 50,
+distanceFilter: 50,
+notificationTitle: 'Background tracking',
+notificationText: 'enabled',
+debug: true,
+startOnBoot: false,
+stopOnTerminate: false,
+locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+interval: 10000,
+fastestInterval: 5000,
+activitiesInterval: 10000,
+stopOnStillActivity: false,
+url: 'http://nickford.com.br/razor/ws.php',
+httpHeaders: {
+'X-FOO': 'bar'
+}
+});
+alert(1);
+BackgroundGeolocation.on('location', (location) => {
+// handle your locations here
+// to perform long running operation on iOS
+// you need to create background task
+BackgroundGeolocation.startTask(taskKey => {
+// execute long running task
+// eg. ajax post location
+// IMPORTANT: task has to be ended by endTask
+ws("bla");
+BackgroundGeolocation.endTask(taskKey);
+});
+});
+
+BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+// handle stationary locations here
+Actions.sendLocation(stationaryLocation);
+});
+
+BackgroundGeolocation.on('error', (error) => {
+ws("error");
+console.log('[ERROR] BackgroundGeolocation error:', error);
+});
+
+BackgroundGeolocation.on('start', () => {
+ws("start");
+console.log('[INFO] BackgroundGeolocation service has been started');
+});
+
+BackgroundGeolocation.on('stop', () => {
+ws("stop");
+console.log('[INFO] BackgroundGeolocation service has been stopped');
+});
+
+BackgroundGeolocation.on('authorization', (status) => {
+ws("auth: "+JSON.stringify(status));
+console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+if (status !== BackgroundGeolocation.AUTHORIZED) {
+Alert.alert('Location services are disabled', 'Would you like to open location settings?', [
+{ text: 'Yes', onPress: () => BackgroundGeolocation.showLocationSettings() },
+{ text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+]);
+}
+});
+
+BackgroundGeolocation.on('background', () => {
+ws("background");
+console.log('[INFO] App is in background');
+});
+
+BackgroundGeolocation.on('foreground', () => {
+ws("foreground");
+console.log('[INFO] App is in foreground');
+});
+alert(2);
+BackgroundGeolocation.checkStatus(status => {
+console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+console.log('[INFO] BackgroundGeolocation service has permissions', status.hasPermissions);
+console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+// you don't need to check status before start (this is just the example)
+if (!status.isRunning) {
+BackgroundGeolocation.start(); //triggers start on start event
+}
+});
+
+BackgroundGeolocation.start();
+alert(3);
 */
